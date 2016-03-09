d763abbfe3c461e56bc1e50aeb1479097c6d406a
Neils01 <iurryy@yahoo.com.br>
2016-03-09 14:30:10 -0300

    adicionando post methodo funcional

--- app/src/main/java/HuntApi/ControleComunicacaoServidor/ControleGeolocalizacao/ControleChamadaPost.java
+++ app/src/main/java/HuntApi/ControleComunicacaoServidor/ControleGeolocalizacao/ControleChamadaPost.java
@@ -6,11 +6,14 @@ import android.util.Log;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 import HuntApi.ControleComunicacaoServidor.UrlChamadaServidor;
 import HuntApi.Model.CordenadaGeografica;
@@ -21,36 +24,38 @@ import HuntApi.Model.CordenadaGeografica;
 public class ControleChamadaPost extends AsyncTask<String, Void, String> {
     CordenadaGeografica cordenada;
     URL url;
-    HttpURLConnection urlConnection = null;
+    URLConnection conn = null;
 
     @Override
     protected String doInBackground(String... strings) {
-
+        byte[] postData;
         try {
 
-            Log.d("Client", strings[0]);
-            URL url = new URL(strings[0]);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setDoInput(true);
-            urlConnection.setDoOutput(true);
-            urlConnection.setRequestProperty("Request-Method", "POST");
-            urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            urlConnection.setRequestProperty("charset", "utf-8");
-            urlConnection.connect();
 
-            OutputStreamWriter writer = new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8");
-            String parametros = URLEncoder.encode("lat", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(cordenada.getLat()), "UTF-8");
-            parametros+= "&" + URLEncoder.encode("lon", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(cordenada.getLon()), "UTF-8");
-            writer.write(URLEncoder.encode(parametros,"UTF-8"));
-            Log.d("Client", parametros);
+            String data = "?" + URLEncoder.encode("lat", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(cordenada.getLat()), "UTF-8");
+            data += "&" + URLEncoder.encode("lon", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(cordenada.getLon()), "UTF-8");
 
-            writer.flush();
-            writer.close();
+            Log.d("Client", strings[0] + data);
+            URL url = new URL(strings[0]+data);
+            //urlConnection.connect();
+
+            conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(data);
+            wr.flush();
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                System.out.println(line);
+            }
+            wr.close();
+            rd.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-
-            urlConnection.disconnect();
         }
         Log.d("Client",cordenada.toString() + "aqui");
         return  null;
--- app/src/main/java/HuntApi/ControleComunicacaoServidor/UrlChamadaServidor.java
+++ app/src/main/java/HuntApi/ControleComunicacaoServidor/UrlChamadaServidor.java
@@ -4,7 +4,7 @@ package HuntApi.ControleComunicacaoServidor;
  * Created by Iury on 1/6/2016.
  */
 public class UrlChamadaServidor {
-    private static final String servidorIPorta = "http://192.168.0.100:8080";
+    private static final String servidorIPorta = "http://10.0.0.9:8081";
     private static final String servidorUrlCordenadas = "/Servidor/cordenada/";
 
     public String getUrlCordenada() {
