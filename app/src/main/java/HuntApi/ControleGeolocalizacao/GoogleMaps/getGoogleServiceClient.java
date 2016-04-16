4b1f382eb42a9957e98f1aea908ee9e67bec1dd8
Neils01 <iurryy@yahoo.com.br>
2016-04-16 20:00:47 -0300

    adicioando metodo para acessar api

--- app/src/main/java/HuntApi/ControleGeolocalizacao/GoogleMaps/getGoogleServiceClient.java
+++ app/src/main/java/HuntApi/ControleGeolocalizacao/GoogleMaps/getGoogleServiceClient.java
@@ -1,7 +1,25 @@
 package HuntApi.ControleGeolocalizacao.GoogleMaps;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 /**
  * Created by Iury on 4/15/2016.
  */
 public class getGoogleServiceClient {
+
+    private GoogleApiClient mGoogleApiClient;
+
+    public GoogleApiClient getmGoogleApiClient() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder()
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            return mGoogleApiClient;
+        } else {
+            return mGoogleApiClient;
+        }
+    }
 }
