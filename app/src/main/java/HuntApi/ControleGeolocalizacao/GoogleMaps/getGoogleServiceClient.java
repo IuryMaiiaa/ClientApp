f2a5c06c3609c6cf680c92d6d0bb9dd3be271a72
Neils01 <iurryy@yahoo.com.br>
2016-04-21 22:29:09 -0300

    agora é possivel pegar a cordenada geografica
    D: é nao foi possivel adicionar o google maps a plataforma, ele tera de
    ser recomendado por fora

--- app/src/main/java/HuntApi/ControleGeolocalizacao/GoogleMaps/getGoogleServiceClient.java
+++ app/src/main/java/HuntApi/ControleGeolocalizacao/GoogleMaps/getGoogleServiceClient.java
@@ -1,9 +1,14 @@
 package HuntApi.ControleGeolocalizacao.GoogleMaps;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+
+import HuntApi.Model.CordenadaGeografica;
 
 /**
  * Created by Iury on 4/15/2016.
@@ -11,6 +16,20 @@ import com.google.android.gms.location.LocationServices;
 public class getGoogleServiceClient {
 
     private GoogleApiClient mGoogleApiClient;
+
+    public GoogleApiClient getGoogleApiClient(Activity activity, GoogleApiClient.ConnectionCallbacks connectionCallBacks,
+                                              GoogleApiClient.OnConnectionFailedListener onConnectionFailedListener) {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(activity)
+                    .addConnectionCallbacks(connectionCallBacks)
+                    .addOnConnectionFailedListener(onConnectionFailedListener)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        return mGoogleApiClient;
+    }
+
+
 
 
 }
--- app/src/main/java/com/example/iury/clientapp/MapsActivity.java
+++ app/src/main/java/com/example/iury/clientapp/MapsActivity.java
@@ -1,6 +1,10 @@
 package com.example.iury.clientapp;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -14,30 +18,24 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
+import HuntApi.Model.CordenadaGeografica;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
-
+    private CordenadaGeografica cordenadaAtual;
     private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
+        cordenadaAtual = new CordenadaGeografica();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
-
 
 
     /**
@@ -61,6 +59,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onConnected(Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            cordenadaAtual.setLat(mLastLocation.getLatitude());
+            cordenadaAtual.setLon(mLastLocation.getLongitude());
+        }
 
     }
 
