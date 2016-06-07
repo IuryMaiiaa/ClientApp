a013cf71dcb1d3819970267f22be82c6726ed7cc
Neils01 <iurryy@yahoo.com.br>
2016-06-07 11:24:00 -0300

    atualziando modelo das quests

--- app/src/main/java/HuntApi/Model/QuestGeolocalizada.java
+++ app/src/main/java/HuntApi/Model/QuestGeolocalizada.java
@@ -17,6 +17,16 @@ public class QuestGeolocalizada {
 
     private CordenadaGeografica cordenada;
 
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
     public Usuario getUsuario() {
         return usuario;
     }
